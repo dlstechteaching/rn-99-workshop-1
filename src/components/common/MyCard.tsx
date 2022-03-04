11a8e7fc878ca2740960571fd09963bdad1d3d73
dls.tech.teaching <dls.tech.teaching@gmail.com>
2022-03-04 19:03:35 +0100

    checking precommit

--- src/components/common/MyCard.tsx
+++ src/components/common/MyCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 
 interface MyCardProps {
 	type: string;
